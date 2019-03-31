70c09512392c894cde177879112e8c628b527007
willgriff95 <will.a.griffiths@gmail.com>
2019-04-01 00:47:49 +0100

    Lloyds_code_test | starting code tests

--- client/src/App/App.test.js
+++ client/src/App/App.test.js
@@ -9,3 +9,21 @@ it('renders without crashing', () => {
 });
 
 // Renders all questions
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
