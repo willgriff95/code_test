a90a84ee456488a7b6437adae7f310a136d71457
Will Griffiths <wilgriff>
2019-04-01 00:11:46 +0100

    Lloyds_code_test | code test functionality all working, need to add code tests and readme

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+var _ = require('lodash');
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/questions', (req,res) => {
+    const questions = require('./Questions/Questions.json');
+    // using lodash chunks to prep the data for pagination
+    // const questionArrayChunks = _.chunk(questionArray.results, 3)
+    res.json(questions);
+    console.log('Questions Retrieved');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
