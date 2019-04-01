122b5f72095704b376053dc036c35b0c10d58e01
willgriff95 <will.a.griffiths@gmail.com>
2019-04-01 10:46:24 +0100

    Lloyds_code_test | adding packages for testing and adding prettier

--- client/src/App/App.test.js
+++ client/src/App/App.test.js
@@ -5,6 +5,12 @@ import axios from "axios";
 import { create } from "react-test-renderer";
 import * as renderer from 'react-test-renderer';
 import questionsJson from '../../../JSON/Questions.json'
+import { mount } from 'enzyme';
+import * as Enzyme from 'enzyme';
+import ReactSixteenAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new ReactSixteenAdapter() });
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
@@ -26,3 +32,9 @@ describe("App component", () => {
     await instance.componentDidMount();
   });
 });
+
+it('correct props are  being passed', () => {  
+  const wrapper = mount(<App includedProp="Success!" excludedProp="I'm not included" />);
+  expect(wrapper.props().includedProp).toEqual('Success!');
+});
+  
